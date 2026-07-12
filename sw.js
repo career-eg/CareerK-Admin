9fc1800bb98306135bcf5c69c4c127d461033211
career-eg <ph.tomasabdelrady@gmail.com>
2026-07-12 23:22:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,55 @@
+const CAREERK_ADMIN_CACHE = 'careerk-admin-pwa-v61';
 
+const ADMIN_ASSETS = [
+  './',
+  './index.html',
+  './install.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CAREERK_ADMIN_CACHE)
+      .then(cache => cache.addAll(ADMIN_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CAREERK_ADMIN_CACHE)
+            .map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+
+  if (request.method !== 'GET') return;
+
+  const url = new URL(request.url);
+
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(request, { cache: 'no-store' })
+      .then(response => {
+        if (response && response.ok) {
+          const copy = response.clone();
+          caches.open(CAREERK_ADMIN_CACHE).then(cache => cache.put(request, copy));
+        }
+
+        return response;
+      })
+      .catch(() => caches.match(request))
+  );
+});
